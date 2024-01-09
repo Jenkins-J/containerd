c7e5d145f99ddbafaf0188e537692c699031e683
James Jenkins <James.Jenkins@ibm.com>
2024-01-09 10:43:13 -0500

    Implement fsverity functionality
    Implement the necessary calls to fsverity for enabling fsverity on a
    file, measuring an fsverity file, and checking if fsverity is enabled
    on a file.
    
    Signed-off-by: James Jenkins <James.Jenkins@ibm.com>

---
+++ pkg/fsverity/fsverity_linux.go
@@ -0,0 +1,108 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package fsverity
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
+
+type fsverityEnableArg struct {
+	version        uint32
+	hash_algorithm uint32
+	block_size     uint32
+	salt_size      uint32
+	salt_ptr       uint64
+	sig_size       uint32
+	reserved1      uint32
+	sig_ptr        uint64
+	reserved2      [11]uint64
+}
+
+type fsverityDigest struct {
+	digest_algorithm uint16
+	digest_size      uint16
+	digest           [64]uint8
+}
+
+const maxDigestSize int = 64
+
+func IsEnabled(path string) (bool, error) {
+	f, err := os.OpenFile(path, os.O_RDONLY, 0644)
+	if err != nil {
+		return false, fmt.Errorf("Error opening file: %s", err)
+	}
+
+	var attr int
+
+	_, _, flagErr := unix.Syscall(syscall.SYS_IOCTL, f.Fd(), uintptr(unix.FS_IOC_GETFLAGS), uintptr(unsafe.Pointer(&attr)))
+	if flagErr != 0 {
+		return false, fmt.Errorf("Error getting inode flags: %s", flagErr)
+	}
+
+	if attr&unix.FS_VERITY_FL == unix.FS_VERITY_FL {
+		return true, nil
+	}
+
+	return false, fmt.Errorf("File %s is not a verity file", path)
+}
+
+func Enable(path string) (bool, error) {
+	f, err := os.OpenFile(path, os.O_RDONLY, 0644)
+	if err != nil {
+		return false, fmt.Errorf("Error opening file: %s\n", err.Error())
+	}
+
+	var args *fsverityEnableArg = &fsverityEnableArg{}
+	args.version = 1
+	args.hash_algorithm = 1
+	// TODO: change block size based on the system page size
+	args.block_size = 65536
+
+	_, _, errno := unix.Syscall(syscall.SYS_IOCTL, f.Fd(), uintptr(unix.FS_IOC_ENABLE_VERITY), uintptr(unsafe.Pointer(args)))
+	if errno != 0 {
+		return false, fmt.Errorf("Enable fsverity failed: %d\n", errno)
+	}
+
+	return true, nil
+}
+
+func Measure(path string) (string, error) {
+	var verityDigest string
+	f, err := os.OpenFile(path, os.O_RDONLY, 0644)
+	if err != nil {
+		fmt.Printf("Error opening file: %s\n", err.Error())
+		return
+	}
+
+	var d *fsverityDigest = &fsverityDigest{digest_size: maxDigestSize}
+	_, _, errno := unix.Syscall(syscall.SYS_IOCTL, f.Fd(), uintptr(unix.FS_IOC_MEASURE_VERITY), uintptr(unsafe.Pointer(d)))
+	if errno != 0 {
+		return verityDigest, fmt.Errorf("Measure fsverity failed: %d\n", errno)
+	}
+
+	var i uint16
+	for i = 0; i < (*d).digest_size; i++ {
+		verityDigest = fmt.Sprintf("%s%x", verityDigest, (*d).digest[i])
+	}
+
+	return verityDigest, nil
+}
