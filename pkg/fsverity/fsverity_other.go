bf8dc694b236e4714a8ab0a157c7ae8d1297f740
James Jenkins <James.Jenkins@ibm.com>
2024-03-28 15:44:46 -0400

    Create fsverity unit tests
    Signed-off-by: James Jenkins <James.Jenkins@ibm.com>

--- pkg/fsverity/fsverity_other.go
+++ pkg/fsverity/fsverity_other.go
@@ -24,6 +24,10 @@ func IsSupported(rootPath string) (bool, error) {
 	return false, fmt.Errorf("fsverity is only supported on Linux systems")
 }
 
+func IsEnabled(path string) (bool, error) {
+	return false, fmt.Errorf("fsverity is only supported on Linux systems")
+}
+
 func Enable(_ string) error {
 	return fmt.Errorf("fsverity is only supported on Linux systems")
 }
